a592cc6bdc6310e9263cf9a9becb553d37ad5ba6
Sardor <sardor.tukhtaev@gmail.com>
2022-11-30 14:31:03 +0500

    Update Program.cs
    changed name of cert and key

--- OpenSSLSigner/Program.cs
+++ OpenSSLSigner/Program.cs
@@ -1,10 +1,10 @@
 ﻿var messageBytes = await File.ReadAllBytesAsync("request.json");
 var signer = new LightSslSigner();
-var signedMessage = signer.Sign(messageBytes, "EZ000000000256.crt", "energymarket.key");
+var signedMessage = signer.Sign(messageBytes, "ADD_YOUR_CERTIFICATE.crt", "ADD_YOUR_KEY.key");
 
 var client = new HttpClient();
 var httpContent = new ByteArrayContent(signedMessage);
 
-var response = await client.PostAsync(" https://test.ofd.uz/emp/receipt", httpContent);
+var response = await client.PostAsync("https://test.ofd.uz/emp/receipt", httpContent);
 
 Console.WriteLine(await response.Content.ReadAsStringAsync());
