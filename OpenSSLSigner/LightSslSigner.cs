83b1b0bfd38c4c6e17bdc9bd4202979ed8aeff5b
david.wahid <david.wahid@gluwa.com>
2022-11-30 14:16:40 +0500

    Add project files.

---
+++ OpenSSLSigner/LightSslSigner.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography.Pkcs;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+public class LightSslSigner
+{
+    public byte[] Sign(byte[] messageBytes, string certPemFile, string keyPemFile, bool includeCertificate = false)
+    {
+        var cert = X509Certificate2.CreateFromPemFile(certPemFile, keyPemFile);
+        var signer = new CmsSigner(cert);
+        var contentInfo = new ContentInfo(messageBytes);
+        var cms = new SignedCms(contentInfo);
+
+        cms.ComputeSignature(signer);
+
+        if(includeCertificate is false)
+            cms.RemoveCertificate(cert);
+
+        return cms.Encode();
+    }
+}
---
+++ OpenSSLSigner/Program.cs
@@ -0,0 +1,10 @@
+﻿var messageBytes = await File.ReadAllBytesAsync("request.json");
+var signer = new LightSslSigner();
+var signedMessage = signer.Sign(messageBytes, "EZ000000000256.crt", "energymarket.key");
+
+var client = new HttpClient();
+var httpContent = new ByteArrayContent(signedMessage);
+
+var response = await client.PostAsync(" https://test.ofd.uz/emp/receipt", httpContent);
+
+Console.WriteLine(await response.Content.ReadAsStringAsync());
